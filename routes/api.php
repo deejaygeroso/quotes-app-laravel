e6bb38f050392771932068c02e017e591c4d268e
Deejay Geroso <deejaygeroso@gmail.com>
2021-08-30 00:08:49 +0800

    Created api CRUDS

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Quote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('quotes', function () {
+    return Quote::all();
+});
+
+Route::get('quotes/{id}', function ($id) {
+    return Quote::find($id);
+});
+
+Route::post('quotes', function (Request $request) {
+    return Quote::create($request->all);
+});
+
+Route::put('quotes/{id}', function (Request $request, $id) {
+    $quote = Quote::findOrFail($id);
+    $quote->update($request->all);
+    return $quote;
+});
+
+Route::delete('quotes/{id}', function ($id) {
+    Quote::find($id)->delete();
+    return 204;
 });
